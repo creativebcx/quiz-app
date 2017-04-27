6ced17d84d9934f5efa42bdd002c586c7f8a6fd2
creativebcx <ericdonley1@gmail.com>
2017-04-26 22:17:13 -0600

    after-work-w-Luis

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var elButton = $('#start-button-handle');
 var elStartButton = $('#start-button');
 var elQuestionArea = $('.question-area');
+var elSubmitButton = $('.submit');
+var elBackButton = $('.back');
+var questionSet = 0;
 var state = {
 	isStarted: false,
 	currentStep: 0,
@@ -39,9 +42,63 @@ function startQuiz() {
 	elStartButton.addClass('hidden');
 	elQuestionArea.removeClass('hidden');
 	state.isStarted = true;
+	newQuestionAnswer();
 }
 //wrapping our logic for hidden classes
 
-$('.submit').click(function(event) {
-	console.log(state.questions[0]);
+elBackButton.click( function(event) {
+	if (questionSet != 0) {
+		questionSet--;
+	};
+	newQuestionAnswer();
 });
+
+elSubmitButton.click( function(event) {
+	if (questionSet != state.questions.length - 1) {
+		questionSet++;
+	};
+	//	elSubmitButton
+	//};
+	console.log('questionSet');
+	newQuestionAnswer();
+});
+
+function newQuestionAnswer() {
+	$('.question-area').html(
+		'<div id="question">' + state.questions[questionSet].text + '</div>' +
+		'<ul class="answer-list">' + 
+			'<li>' + '<input name="answer" type="radio" class="answer-1"/>' + '<span>' + state.questions[questionSet].answer[0] + '</span>' + '</li>' +
+			'<li>' + '<input name="answer" type="radio" class="answer-2"/>' + '<span>' + state.questions[questionSet].answer[1] + '</span>' + '</li>' +
+			'<li>' + '<input name="answer" type="radio" class="answer-3"/>' + '<span>' + state.questions[questionSet].answer[2] + '</span>' + '</li>' +
+			'<li>' + '<input name="answer" type="radio" class="answer-4"/>' + '<span>' + state.questions[questionSet].answer[3] + '</span>' + '</li>' +
+			'</ul>');
+	if (validateAnswer(2, questionSet)) {
+		$('.message-area').html(
+			state.questions[questionSet].correctAnswer[1]
+	)}
+	else {
+		$('.message-area').html(
+			"incorrect answer"
+		)};
+};
+
+function validateAnswer(answerIndex, questionIndex) {
+	return state.questions[questionIndex].correctAnswer[0] == answerIndex;
+};
+
+
+console.log(validateAnswer(3,0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
